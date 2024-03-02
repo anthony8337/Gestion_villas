7cc74aefded199b35cb5689b9dce1383695e475b
anthony8337 <anthonyoliva70@gmail.com>
2024-03-02 01:03:04 -0600

    2 de marzo
    20 días para la defensa
    
    Tengo mello ( o_o)

--- Plataforma de gestion de villas palma real/Tablas/tabla_cliente.php
+++ Plataforma de gestion de villas palma real/Tablas/tabla_cliente.php
@@ -6,18 +6,21 @@
 <table id='tabla_cliente'>
 <?php
 
+include "Tablas/datos_random.php";
+
+
 $letra = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 $estadia;
 
 echo"
 
 <tr>
-<th>",'Villa',"</th>
-<th>",'Nombre Completo',"</th>
-<th>",'RTN',"</th>
-<th>",'Numero telefonico',"</th>
-<th>",'Estado',"</th>
-<th>",'Fecha de ingreso',"</th>
+<th>Villa</th>
+<th>Nombre Completo</th>
+<th>RTN</th>
+<th>Numero telefonico</th>
+<th>Estado</th>
+<th>Fecha de ingreso</th>
 </tr>";
 $i = 0;
 while($i < count($letra))
@@ -37,11 +40,12 @@ while($i < count($letra))
         echo "
         <tr class='contenido' onclick='modificar_cliente()'>
         <td>",$letra[$i],'-',$j,"</td>
-        <td>",$letra[$i],'nthony ',$letra[$i],'lemente ',$letra[$i],'liva ',$letra[$i],'urillo',"</td>
-        <td>",'0101199',$j,"03209</td>
-        <td>",'94856463',"</td>
+        <td>",$nombre[rand(0,count($nombre)-1)]," ",$Snombre[rand(0,count($Snombre)-1)],
+        $apellido[rand(0,count($apellido)-1)],$sapellido[rand(0,count($sapellido)-1)],"</td>
+        <td>0101-",rand(1950,2000),"-",rand(10000,50000),"</td>
+        <td>+504 ",rand(80000000,99999999),"</td>
         <td>",$estadia[$j],"</td>
-        <td>",'2024-08-12',"</td>
+        <td>",rand(2000,2024),"-",rand(01,12),"-",rand(1,31),"</td>
         </tr>
         ";
     }
--- Plataforma de gestion de villas palma real/Tablas/tabla_principal.php
+++ Plataforma de gestion de villas palma real/Tablas/tabla_principal.php
@@ -8,17 +8,22 @@
 <table id="tabla"> 
 
 
+
 <tr>
+
 <th>Villa</th>
-<th>Propietario</th>
-<th>Estatus</th>
-<th>RTN</th>
-<th>Telefono</th>
 <th>Medidor EEH</th>
+<th>Nombre Completo</th>
+<th>RTN</th>
+<th>Numero telefonico</th>
+<th>Estado</th>
+<th>Fecha de ingreso</th>
 <th>Saldo</th>
 </tr>
 
 <?php
+
+include "Tablas/datos_random.php";
 
 $letra = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
@@ -31,14 +36,25 @@ while($i < count($letra))
 
 for($j = 1; $j <= 12; $j++)
 {
+    if($j == 2)
+        {
+            $estadia[2] = "Suspendido";
+        }
+        else
+        {
+            $estadia[$j] = "Aplica cuota";
+        }
+    
     echo "
     <tr class='contenido'>
     <td>",$letra[$i],"-",$j,"<label style='display: none;'>.</label></td>
-    <td>Anthony Oliva</td>
-    <td>Activo</td>
-    <td>0101-199",$j,"-03209</td>
-    <td>+504 94856463</td>
     <td>45628",$j,"</td>
+    <td>",$nombre[rand(0,count($nombre)-1)]," ",$Snombre[rand(0,count($Snombre)-1)]," ",
+    $apellido[rand(0,count($apellido)-1)]," ",$sapellido[rand(0,count($sapellido)-1)],"</td>
+    <td>0101-",rand(1950,2000),"-",rand(10000,50000),"</td>
+    <td>+504 ",rand(80000000,99999999),"</td>
+    <td>",$estadia[$j],"</td>
+    <td>",rand(2000,2024),"-",rand(01,12),"-",rand(1,31),"</td>
     <td>$ 4",$j,"</td>
     </tr>
     ";
