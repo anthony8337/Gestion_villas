9262d860a1790110ce397976565c376a3200e056
Anthony Clemente Oliva Murillo <69278345+anthony8337@users.noreply.github.com>
2024-03-15 21:25:19 -0600

    15 de marzo

--- Gestion_propietario/z_pruebas.php
+++ Gestion_propietario/z_pruebas.php
@@ -7,13 +7,7 @@ include "1_Raiz/cabezal.php";
     <span onclick="cerrar_inser_cliente()" class="cerrar">&times;</span>
 
     <br>
-    <button>
-        Guardar
-    </button>
 
-    <button>
-        Cancelar
-    </button>
 
     <button style="float: inline-end;">
         Imprimir
@@ -21,6 +15,7 @@ include "1_Raiz/cabezal.php";
 
     <br>
 
+    <fieldset style="padding: 0; width: 100%; border: 0px;">
     <fieldset class="dat_saldo" style="width: 50%; margin-top: 20px;">
     <legend>Codigo: <Label>A-5</Label></legend>
 
@@ -74,9 +69,10 @@ include "1_Raiz/cabezal.php";
      </table>
 
     </fieldset>
+    </fieldset>
 
     <div class="cuerpo_tabla">
-    <table>
+    <table id="tabla_contenido">
 
         <tr>
             <th>Documento</th>
@@ -92,7 +88,7 @@ include "1_Raiz/cabezal.php";
         <?php
         
         $costo = 160;
-        $abono = 3200;
+        $abono = 800;
         $importe = $costo;
 
         $_POST["total"] = 5;
@@ -112,7 +108,7 @@ include "1_Raiz/cabezal.php";
 
             echo"
             <tr>
-            <td>A1234</td>
+            <td>A1234$i</td>
             <td>Pago Mensual</td>
             <td>03/03/2024</td>
             <td>03/04/2024</td>
@@ -122,12 +118,18 @@ include "1_Raiz/cabezal.php";
             <td>$saldo</td>
             </tr>
             ";
-        
+
+            $saldo += $i;
         }
+
+        
+
         ?>
 
     </table>
     </div>
+
+    
 
     <fieldset class="dat_saldo" style="height: 65px; width: 40%; margin-top: 20px; float: inline-end;">
     <legend>Total de saldo</legend>
@@ -135,7 +137,7 @@ include "1_Raiz/cabezal.php";
      <table class="datos_b_sal">
         <tr>
             <td>
-            <input id="total" type="text">
+                <label><?php echo $saldo;?></label>
             </td>
         </tr>
      </table>
