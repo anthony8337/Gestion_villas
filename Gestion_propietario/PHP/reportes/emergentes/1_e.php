d1271f68b1d4fb2f5503e7c7c9f3b2a33f1a0056
Anthony Clemente Oliva Murillo <69278345+anthony8337@users.noreply.github.com>
2024-09-02 04:41:39 -0600

    2 de sep
    casi casi

--- Gestion_propietario/PHP/reportes/emergentes/1_e.php
+++ Gestion_propietario/PHP/reportes/emergentes/1_e.php
@@ -9,5 +9,55 @@
 <button onclick="cerrar_ingreso_lv()" type="button">X</button>
 </div>
 
+<div class="c2">
+    <fieldset>
+            <legend>Desde</legend>
+            <select name="desde_reportes_villas" id="desde_reportes_villas"></select>
+        </fieldset>
+
+        <fieldset>
+            <legend>Hasta</legend>
+            <select name="hasta_reportes_villas" id="hasta_reportes_villas"></select>
+        </fieldset>
+
+    <fieldset>
+        <legend>Estado</legend>
+
+        <select name="estado_villa" id="estado_villa"></select>
+
+    </fieldset>
+
+    <fieldset>
+        <legend>Modelo</legend>
+
+        <select name="modelo_villa" id="modelo_villa"></select>
+
+    </fieldset>
+</div>
+
+<div class="c4">
+<div class="contenido_tabla">
+    <table id="td_villas_cuerpo" class="contenido">
+        <tr>
+        <th></th>
+        <th>Villas</th>
+        <th>Número de contador</th>
+        <th>Habitación</th>
+        <th>Tamaño de lote</th>
+        <th>Modelo</th>
+        <th>Estado</th>
+        </tr>
+
+        <tbody id="tb_villas">
+        </tbody>
+
+    </table>
+    </div>
+</div>
+
+<div class="c3">
+<button type="button" onclick="abrirNuevaPagina_estado()">Imprimir</button>
+</div>
+
 </div>
 </div>
